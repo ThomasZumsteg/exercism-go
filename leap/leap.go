33cc5ad3658387e7e83e5c07d95f16b836453d5a
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-11-23 11:41:21 -0800

    Update comment style in leap

--- leap/leap.go
+++ leap/leap.go
@@ -2,10 +2,12 @@ package leap
 
 const TestVersion = 1
 
-// True if the year is a leap year
-// leap years are evenly divisible by 4
-// unless they are evenly divisible by 100
-// and not evenly divisible by 400
+/*
+True if the year is a leap year
+leap years are evenly divisible by 4
+unless they are evenly divisible by 100
+and not evenly divisible by 400
+*/
 func IsLeapYear(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
