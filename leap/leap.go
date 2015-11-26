713569dc03fb3fcbac459e3df2faf99421e432cf
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-11-26 14:34:53 -0800

    Updated doc strings in leap to be more go like

--- leap/leap.go
+++ leap/leap.go
@@ -1,15 +1,12 @@
 package leap
 
-// TestVersion Version of the test suite that the program was build against
+// TestVersion is the version test suite the program is designed to pass.
 const TestVersion = 1
 
 /*
-IsLeapYear
-Returns true if the "year" is a leap year, false otherwise
-
-Years are determined by the following rules
-The year is evenly divisble by 4 and not by 100
-unless it's evenly divisible by 400
+IsLeapYear determines if a year is a leap year according the following rule:
+Leap years are evenly divisble by 4 and not by 100
+unless also divisible by 400.
 */
 func IsLeapYear(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
