4129c8d03601d7f9375e6d336243a7c2ad6c0f71
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-12-02 11:31:38 -0800

    Updated gigasecond docs

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,19 +1,18 @@
 package gigasecond
 
 import (
-    "time"
-    "math"
+	"math"
+	"time"
 )
 
-// TestVersion the version of tests to run
-const TestVersion = 2 
+// TestVersion is the version of the unit tests was designed to pass.
+const TestVersion = 2
 
-// AddGigasecond adds one gigasecond to the date
+/*AddGigasecond adds one gigasecond (1E9 seconds) to the date.*/
 func AddGigasecond(today time.Time) time.Time {
-    GIGASECOND := time.Duration(math.Pow(10, 9))
-    return today.Add(GIGASECOND * time.Second)
+	GIGASECOND := time.Duration(math.Pow(10, 9))
+	return today.Add(GIGASECOND * time.Second)
 }
 
-// Birthday my birthday to compute a gigasecond
+// Birthday is my birthday and is used to compute a giga-versery.
 var Birthday, _ = time.Parse("2006-01-02", "1941-12-07")
-
