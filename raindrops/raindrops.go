f0d97c66290d799fd3e204391ea187c13d325121
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-12-02 11:43:49 -0800

    Updated raindrops to use an array

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -5,25 +5,26 @@ import "fmt"
 // TestVersion the version of the tests to run
 const TestVersion = 1
 
-/* Convert converts a number to raindrop sounds
+// rain drop sounds
+var rain = make([]string, 10)
+
+/*Convert converts a number to raindrop sounds
 Adds Pling if the number is divisible by 3
      Plang if the number is divisible by 5
      Plong if the number is divisible by 7
      Otherwise returns the number as a string */
 func Convert(num int) string {
-    sounds := ""
-    if num % 3 == 0 {
-        sounds += "Pling"
-    }
-    if num % 5 == 0 {
-        sounds += "Plang"
-    }
-    if num % 7 == 0 {
-        sounds += "Plong"
-    }
-    if sounds == "" {
-        sounds = fmt.Sprintf("%d", num)
-    }
-    return sounds
+	sounds := ""
+	rain[3] = "Pling"
+	rain[5] = "Plang"
+	rain[7] = "Plong"
+	for i, sound := range rain {
+		if 0 < i && num%i == 0 {
+			sounds += sound
+		}
+	}
+	if sounds == "" {
+		sounds = fmt.Sprintf("%d", num)
+	}
+	return sounds
 }
-
