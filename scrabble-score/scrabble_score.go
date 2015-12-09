916d08ccd81142e50ee60cfd1ac7e024beafc2c8
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-12-09 10:23:02 -0800

    Updated scrabble-score to use hardcoded map

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -5,37 +5,20 @@ import "strings"
 /*Score determines the value of a scrabble word*/
 func Score(word string) int {
 	score := 0
-	letterValues := makeMap()
 	for _, l := range strings.ToUpper(word) {
-		if v, ok := letterValues[l]; ok {
-			score += v
-		}
+		score += letterScores[l]
 	}
 	return score
 }
 
-/*makeMap builds a letter score map for a game of scrabble*/
-func makeMap() map[rune]int {
-	letterMap := map[rune]int{}
-	for value, letters := range letterScores {
-		for _, letter := range letters {
-			letterMap[letter] = value
-		}
-	}
-	return letterMap
-}
-
 //letterScores sets the value of letters
-var letterScores = []string{
-	"",
-	"AEIOULNRST",
-	"DG",
-	"BCMP",
-	"FHVWY",
-	"K",
-	"",
-	"",
-	"JX",
-	"",
-	"QZ",
+var letterScores = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1,
+	'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
 }
