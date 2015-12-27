353c37187a36261cc5f6e4792742b8094747c737
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-12-27 10:46:45 -0800

    First iteration circular-buffer

---
+++ circular-buffer/circular_buffer.go
@@ -0,0 +1,55 @@
+package circular
+
+import "errors"
+
+//TestVersion is the unit tests that pass.
+const TestVersion = 2
+
+//Buffer is a circular ring buffer.
+type Buffer struct {
+	read, used int
+	data       []byte
+}
+
+/*NewBuffer creates a new ring buffer of a certain size.*/
+func NewBuffer(size int) *Buffer {
+	var buff = Buffer{0, 0, make([]byte, size)}
+	return &buff
+}
+
+/*ReadByte reads the oldest byte in the buffer,
+cant read an empty buffer.*/
+func (b *Buffer) ReadByte() (byte, error) {
+	if b.used == 0 {
+		return 0, errors.New("Buffer is empty")
+	}
+	read := b.data[b.read]
+	b.read = (b.read + 1) % len(b.data)
+	b.used--
+	return read, nil
+}
+
+/*WriteByte writes to the buffer and woun't write to a full buffer.*/
+func (b *Buffer) WriteByte(c byte) error {
+	if b.used == len(b.data) {
+		return errors.New("Buffer is full")
+	}
+	write := (b.read + b.used) % len(b.data)
+	b.data[write] = c
+	b.used++
+	return nil
+}
+
+/*Overwrite writes to buffer even if it's full*/
+func (b *Buffer) Overwrite(c byte) {
+	if b.used == len(b.data) {
+		b.ReadByte()
+	}
+	b.WriteByte(c)
+}
+
+/*Reset clears the buffer of all data.*/
+func (b *Buffer) Reset() {
+    //Doesn't actually clear the buffer
+	b.used = 0
+}
---
+++ circular-buffer/circular_buffer_test.go
@@ -0,0 +1,213 @@
+package circular
+
+// Implement a circular buffer of bytes supporting both overflow-checked writes
+// and unconditional, possibly overwriting, writes.
+//
+//   type Buffer
+//   func NewBuffer(size int) *Buffer
+//   func (*Buffer) ReadByte() (byte, error)
+//   func (*Buffer) WriteByte(c byte) error
+//   func (*Buffer) Overwrite(c byte)
+//   func (*Buffer) Reset() // put buffer in an empty state
+//
+// We chose the above API so that Buffer implements io.ByteReader
+// and io.ByteWriter and can be used (size permitting) as a drop in
+// replacement for anything using that interface.
+
+import (
+	"io"
+	"testing"
+)
+
+const testVersion = 2
+
+// Retired:
+// (non-versioned) ea3c43868a02d8d899d3a99163380a3b7b3a0a18
+// 1               38c982fc7e6861dfebec2a0023a85e53ea8c1b59
+
+func TestTestVersion(t *testing.T) {
+	if TestVersion != testVersion {
+		t.Errorf("Found TestVersion = %v, want %v.", TestVersion, testVersion)
+	}
+}
+
+// Here is one way you can have a test case verify that the expected
+// interfaces are implemented.
+
+var _ io.ByteReader = new(Buffer)
+var _ io.ByteWriter = new(Buffer)
+
+// testBuffer and methods support the tests, providing log and fail messages.
+
+type testBuffer struct {
+	*testing.T
+	b *Buffer
+}
+
+func nb(size int, t *testing.T) testBuffer {
+	t.Logf("NewBuffer(%d)", size)
+	return testBuffer{t, NewBuffer(size)}
+}
+
+func (tb testBuffer) read(want byte) {
+	switch c, err := tb.b.ReadByte(); {
+	case err != nil:
+		tb.Fatalf("ReadByte() failed unexpectedly: %v", err)
+	case c != want:
+		tb.Fatalf("ReadByte() = %c, want %c.", c, want)
+	}
+	tb.Logf("ReadByte %c", want)
+}
+
+func (tb testBuffer) readFail() {
+	if c, err := tb.b.ReadByte(); err == nil {
+		tb.Fatalf("ReadByte() = %c, expected a failure", c)
+	}
+	tb.Log("ReadByte() fails as expected")
+}
+
+func (tb testBuffer) write(c byte) {
+	if err := tb.b.WriteByte(c); err != nil {
+		tb.Fatalf("WriteByte(%c) failed unexpectedly: %v", c, err)
+	}
+	tb.Logf("WriteByte(%c)", c)
+}
+
+func (tb testBuffer) writeFail(c byte) {
+	if err := tb.b.WriteByte(c); err == nil {
+		tb.Fatalf("WriteByte(%c) succeeded, expected a failure", c)
+	}
+	tb.Logf("WriteByte(%c) fails as expected", c)
+}
+
+func (tb testBuffer) reset() {
+	tb.b.Reset()
+	tb.Log("Reset()")
+}
+
+func (tb testBuffer) overwrite(c byte) {
+	tb.b.Overwrite(c)
+	tb.Logf("Overwrite(%c)", c)
+}
+
+// tests.  separate functions so log will have descriptive test name.
+
+func TestReadEmptyBuffer(t *testing.T) {
+	tb := nb(1, t)
+	tb.readFail()
+}
+
+func TestWriteAndReadOneItem(t *testing.T) {
+	tb := nb(1, t)
+	tb.write('1')
+	tb.read('1')
+	tb.readFail()
+}
+
+func TestWriteAndReadMultipleItems(t *testing.T) {
+	tb := nb(2, t)
+	tb.write('1')
+	tb.write('2')
+	tb.read('1')
+	tb.read('2')
+	tb.readFail()
+}
+
+func TestReset(t *testing.T) {
+	tb := nb(3, t)
+	tb.write('1')
+	tb.write('2')
+	tb.write('3')
+	tb.reset()
+	tb.write('1')
+	tb.write('3')
+	tb.read('1')
+	tb.write('4')
+	tb.read('3')
+}
+
+func TestAlternateWriteAndRead(t *testing.T) {
+	tb := nb(2, t)
+	tb.write('1')
+	tb.read('1')
+	tb.write('2')
+	tb.read('2')
+}
+
+func TestReadOldestItem(t *testing.T) {
+	tb := nb(3, t)
+	tb.write('1')
+	tb.write('2')
+	tb.read('1')
+	tb.write('3')
+	tb.read('2')
+	tb.read('3')
+}
+
+func TestWriteFullBuffer(t *testing.T) {
+	tb := nb(2, t)
+	tb.write('1')
+	tb.write('2')
+	tb.writeFail('A')
+}
+
+func TestOverwriteFull(t *testing.T) {
+	tb := nb(2, t)
+	tb.write('1')
+	tb.write('2')
+	tb.overwrite('A')
+	tb.read('2')
+	tb.read('A')
+	tb.readFail()
+}
+
+func TestOverwriteNonFull(t *testing.T) {
+	tb := nb(2, t)
+	tb.write('1')
+	tb.overwrite('2')
+	tb.read('1')
+	tb.read('2')
+	tb.readFail()
+}
+
+func TestAlternateReadAndOverwrite(t *testing.T) {
+	tb := nb(5, t)
+	tb.write('1')
+	tb.write('2')
+	tb.write('3')
+	tb.read('1')
+	tb.read('2')
+	tb.write('4')
+	tb.read('3')
+	tb.write('5')
+	tb.write('6')
+	tb.write('7')
+	tb.write('8')
+	tb.overwrite('A')
+	tb.overwrite('B')
+	tb.read('6')
+	tb.read('7')
+	tb.read('8')
+	tb.read('A')
+	tb.read('B')
+	tb.readFail()
+}
+
+func BenchmarkOverwrite(b *testing.B) {
+	c := NewBuffer(100)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.Overwrite(0)
+	}
+	b.SetBytes(int64(b.N))
+}
+
+func BenchmarkWriteRead(b *testing.B) {
+	c := NewBuffer(100)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.WriteByte(0)
+		c.ReadByte()
+	}
+	b.SetBytes(int64(b.N))
+}
