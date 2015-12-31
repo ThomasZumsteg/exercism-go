2fa764052969fb1899fe0f50ac8ae1a6d6223503
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-12-30 22:11:23 -0800

    Still working on react

--- react/react.go
+++ react/react.go
@@ -1,6 +1,6 @@
 package react
 
-import "fmt"
+// import "fmt"
 
 const TestVersion = 3
 
@@ -8,7 +8,7 @@ type sheet []cell
 type callback *func(int)
 
 type cell struct {
-    updateList []callback
+    callbacks []callback
     value int
 }
 
@@ -21,26 +21,26 @@ func (c *cell) SetValue(val int) {
         return
     }
     c.value = val
-    for _, updateID := range c.updateList {
-        update := *updateID
-        update(val)
+    for _, callID := range c.callbacks{
+        call := *callID
+        call(val)
     }
 }
 
-func (c *cell) AddCallback(f func(int)) CallbackHandle {
-    c.updateList = append(c.updateList, &f)
-    return &f
+func (c *cell) AddCallback(call func(int)) CallbackHandle {
+    c.callbacks = append(c.callbacks, &call)
+    return &call
 }
 
 func (c *cell) RemoveCallback(remove CallbackHandle) {
-    var newList []callback
-    for _, handle := range c.updateList {
-        if handle != remove.(callback) {
-            fmt.Printf("Checking %s != %s\n", handle, remove)
-            newList = append(newList, handle)
+    var newCallBacks []callback
+    removeAddr := callback(remove.(*func(int)))
+    for _, handle := range c.callbacks {
+        if handle != removeAddr {
+            newCallBacks = append(newCallBacks, handle)
         }
     }
-    c.updateList = newList
+    c.callbacks = newCallBacks
 }
 
 func New() Reactor {
@@ -48,20 +48,20 @@ func New() Reactor {
 }
 
 func (s *sheet) CreateInput(val int) InputCell {
-    c := cell{}
-    c.SetValue(val)
-    *s = append(*s, c)
-    return &c
+    inputCell := cell{}
+    inputCell.SetValue(val)
+    *s = append(*s, inputCell)
+    return &inputCell
 }
 
 func (s *sheet) CreateCompute1(c Cell, f func(int)int) ComputeCell {
     inputCell := c.(*cell)
     computeCell := cell{}
-    var update = func(n int) {
-        computeCell.SetValue(f(n))
+    var update = func(_ int) {
+        computeCell.SetValue(f(inputCell.Value()))
     }
-    update(inputCell.Value())
-    inputCell.updateList = append(inputCell.updateList, &update)
+    inputCell.AddCallback( update )
+    update(0)
     return &computeCell
 }
 
@@ -69,17 +69,13 @@ func (s *sheet) CreateCompute2(c1, c2 Cell, f func(int, int)int) ComputeCell {
     inputCell1 := c1.(*cell)
     inputCell2 := c2.(*cell)
     computeCell := cell{}
-    var update1 = func(n int) {
-        computeCell.SetValue(f(n, inputCell2.Value()))
-    }
-    update1(inputCell1.Value())
-    inputCell1.updateList = append(inputCell1.updateList, &update1)
 
-    var update2 = func(n int) {
-        computeCell.SetValue(f(inputCell1.Value(), n))
+    var update = func(_ int) {
+        computeCell.SetValue(f(inputCell1.Value(), inputCell2.Value()))
     }
-    update2(inputCell2.Value())
-    inputCell2.updateList = append(inputCell2.updateList, &update2)
+    update(0)
+    inputCell1.AddCallback( update )
+    inputCell2.AddCallback( update )
 
     return &computeCell
 }
