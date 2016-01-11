514cd8f35bc11526a4f01ced09489dd0bc0c1a77
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-01-10 17:47:52 -0800

    First iteration octal

---
+++ octal/octal.go
@@ -0,0 +1,21 @@
+package octal
+
+import (
+	"errors"
+	"fmt"
+)
+
+/*ParseOctal converts a string of octal digits to a decimal number.*/
+func ParseOctal(oct string) (int64, error) {
+	var dec int64
+	for i, octDigit := range oct {
+		if octDigit < '0' || '7' < octDigit {
+			err := fmt.Sprintf("Not valid character: %s[%s]%s",
+				oct[:i], oct[i], oct[i+1:])
+			return 0, errors.New(err)
+		}
+		dec *= 8
+		dec += int64(octDigit - '0')
+	}
+	return dec, nil
+}
