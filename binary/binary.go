f3247d857f61b1d570c70f9f0a25fb936216534f
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-01-18 14:24:57 -0800

    Updated print line in binary to print rune instead of string

--- binary/binary.go
+++ binary/binary.go
@@ -14,7 +14,7 @@ func ParseBinary(bin string) (int, error) {
 		n, ok := strconv.Atoi(string(v))
 		switch {
 		case ok != nil:
-			msg := fmt.Sprintf("\"%s\" is not a vaid digit", v)
+			msg := fmt.Sprintf("\"%c\" is not a vaid digit", v)
 			return 0, errors.New(msg)
 		case n < 0 || 1 < n:
 			msg := fmt.Sprintf("\"%d\" is not a binary digit", n)
