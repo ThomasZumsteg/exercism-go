3850fb58048fd644f5aba6a0f59efaed01fd6621
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-01-25 15:27:45 -0800

    Updated largest series product for V2 tests

--- largest-series-product/largest_series_product.go
+++ largest-series-product/largest_series_product.go
@@ -1,29 +1,30 @@
 package lsproduct
 
 import (
-	"errors"
 	"fmt"
 	"strconv"
 )
 
 //TestVersion is the unit tests that these functions are designed to pass
-const TestVersion = 1
+const TestVersion = 2
 
 /*LargestSeriesProduct finds the largest product of a sequence of digits
 of a given length, it also checks the lengths are possible*/
 func LargestSeriesProduct(number string, span int) (int, error) {
-	if len(number) < span {
-		errMsg := fmt.Sprintf("Span (%d) needs to be smaller than the number (%s)", span, number)
-		return 0, errors.New(errMsg)
+	switch {
+	case len(number) < span:
+		return 0, fmt.Errorf("Span (%d) needs to be smaller than the number (%s)", span, number)
+	case span < 0:
+		return 0, fmt.Errorf("Span needs to be positive: %d", span)
 	}
+
 	largest := 0
 	for start := 0; start <= len(number)-span; start++ {
 		slice := 1
 		for i := 0; i < span; i++ {
 			n, err := strconv.Atoi(string(number[i+start]))
 			if err != nil {
-				errMsg := fmt.Sprintf("%s(%c) does appear to be a valid digit", number[:i], number[i])
-				return 0, errors.New(errMsg)
+				return 0, fmt.Errorf("%s(%c) does appear to be a valid digit", number[:i], number[i])
 			}
 			slice *= n
 		}
