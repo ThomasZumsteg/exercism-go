15c58567af10e043e16a4a1439985afbc5a936c5
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-05-18 10:33:45 -0700

    First iteration reverse-string

---
+++ reverse-string/cases_test.go
@@ -0,0 +1,39 @@
+package reverse
+
+// Source: exercism/problem-specifications
+// Commit: 2f77985 reverse-string: apply "input" policy
+// Problem Specifications Version: 1.1.0
+
+type reverseTestCase struct {
+	description string
+	input       string
+	expected    string
+}
+
+var testCases = []reverseTestCase{
+	{
+		description: "an empty string",
+		input:       "",
+		expected:    "",
+	},
+	{
+		description: "a word",
+		input:       "robot",
+		expected:    "tobor",
+	},
+	{
+		description: "a capitalized word",
+		input:       "Ramen",
+		expected:    "nemaR",
+	},
+	{
+		description: "a sentence with punctuation",
+		input:       "I'm hungry!",
+		expected:    "!yrgnuh m'I",
+	},
+	{
+		description: "a palindrome",
+		input:       "racecar",
+		expected:    "racecar",
+	},
+}
