e148126824b6ac3f0c19480f4df7172fcdbadb91
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-10-12 04:35:27 +0000

    Work on zebra puzzle

--- zebra-puzzle/zebra_puzzle.go
+++ zebra-puzzle/zebra_puzzle.go
@@ -1,99 +1,89 @@
 package zebra
 
-// import "fmt"
+import (
+    "strings"
+    "strconv"
+    // "fmt"
+)
+
+type Rule func(House) bool
 
 type Solution struct {
 	DrinksWater string
 	OwnsZebra   string
 }
 
-type House struct {
-    Position int
-	Owner string
-	Pet   string
-	Color string
-	Drink string
-	Smoke string
-}
+type House map[string]string
 
-func makeHouses(numHouses int) []House {
+func makeHouses(attribute_map map[string][]string) []House {
     houses := []House{}
-    for h := 0; h < numHouses; h++ {
-        houses = append(houses, House{ -1, "", "", "", "", ""})
+    for h := 0; h < 5; h++ {
+        house := make(map[string]string)
+        house["Position"] = strconv.Itoa(h)
+        houses = append(houses, house)
+    }
+    for attr, values := range attribute_map {
+        new_houses := []House{}
+        for _, value := range values {
+            for _, house := range houses {
+                new_house := house
+                house[attr] = value
+                new_houses = append(new_houses, new_house)
+            }
+        }
+        houses = new_houses
     }
     return houses
 }
 
-func SolvePuzzle() Solution {
-    // 1. There are five houses.
-    houses := makeHouses(5)
-	var rules = [](func([]House) bool){
-		// 2. The Englishman lives in the red house.
-		makeRule("Owner", "English", "Color", "Red"),
-		// 3. The Spaniard owns the dog.
-		makeRule("Owner", "Spanish", "Pet", "Dog"),
-		// 4. Coffee is drunk in the green house.
-		makeRule("Drink", "Coffee", "Color", "Green"),
-		// 5. The Ukrainian drinks tea.
-		// 7. The Old Gold smoker owns snails.
-		makeRule("Smoke", "Old Gold", "Pet", "Snail"),
-		// 8. Kools are smoked in the yellow house.
-		makeRule("Smoke", "Kools", "Color", "Yellow"),
-		// 9. Milk is drunk in the middle house.
-		makeRule("Position", 2, "Drink", "Milk"),
-		// 10. The Norwegian lives in the first house.
-		makeRule("Position", 0, "Owner", "Norwegian"),
-		// 13. The Lucky Strike smoker drinks orange juice.
-		makeRule("Smoke", "Luck Strike", "Drink", "Orange Juice"),
-		// 14. The Japanese smokes Parliaments.
-		makeRule("Smoke", "Parliaments", "Owner", "Japanese"),
-	}
-
-    var checks = [](func([]House) bool) {
-		// 11. The man who smokes Chesterfields lives in the house next to the man with the fox.
-		func(houses []House) bool {
-			for h, house := range houses {
-				if house.Smoke == "Chesterfields" {
-					return (0 < h && houses[h-1].Pet == "Fox") ||
-						(h < 4 && houses[h+1].Pet == "Fox")
-				}
-			}
-            return false
-		},
-		// 12. Kools are smoked in the house next to the house where the horse is kept.
-		func(houses []House) bool {
-			for h, house := range houses {
-				if house.Smoke == "Kools" {
-					return (0 < h && houses[h-1].Pet == "Horse") ||
-						(h < 4 && houses[h+1].Pet == "Horse")
-				}
-			}
-            return false
-		},
-		// 15. The Norwegian lives next to the blue house.
-		func(houses []House) bool {
-			for h, house := range houses {
-				if house.Owner == "Norwegian" {
-					return (0 < h && houses[h-1].Color == "Blue") ||
-						(h < 4 && houses[h+1].Color == "Blue")
-				}
-			}
-            return false
-		},
+func makeRule(key1, value1, key2, value2, position string) Rule {
+    return func(house House) bool {
+        return false
     }
+}
 
+func SolvePuzzle() Solution {
 	var values = map[string][]string{
-		"Owners": []string{
-			"English", "Spanish", "Ukrainian", "Norwegian", "Japanses"},
-		"Pets": []string{
-			"Dog", "Snail", "Fox", "Horse", "Zebra"},
-		"Drink": []string{
-			"Water", "Milk", "Tea", "Orange Juice", "Coffee"},
-		"Smokes": []string{
-			"Kools", "Old Gold", "Chesterfields", "Lucky Strike", "Parliament"},
-		"Color": []string{
-			"Red", "Green", "Ivory", "Yellow", "Blue"},
+		"Owners": strings.Split("English,Spanish,Ukrainian,Norwegian,Japanses", ","),
+		"Pets": strings.Split("Dog,Snail,Fox,Horse,Zebra", ","),
+        "Drink": strings.Split("Water,Milk,Tea,Orange Juice,Coffee", ","),
+		"Smokes": strings.Split("Kools,Old Gold,Chesterfields,Lucky Strike,Parliament", ","),
+		"Color": strings.Split("Red,Green,Ivory,Yellow,Blue", ","),
 	}
+
+    houses := makeHouses(values)
+	var rules = [](Rule){
+		// 2. The Englishman lives in the red house.
+		makeRule("Owner", "English", "Color", "Red", "same"),
+		// 3. The Spaniard owns the dog.
+		makeRule("Owner", "Spanish", "Pet", "Dog", "same"),
+		// 4. Coffee is drunk in the green house.
+		makeRule("Drink", "Coffee", "Color", "Green", "same"),
+		// 5. The Ukrainian drinks tea.
+		makeRule("Owner", "Ukrainian", "Drink", "Tea", "same"),
+        // 6. The green house is immediately to the right of the ivory house.
+		makeRule("Color", "Green", "Color", "Ivory", "right"),
+		// 7. The Old Gold smoker owns snails.
+		makeRule("Smoke", "Old Gold", "Pet", "Snail", "same"),
+		// 8. Kools are smoked in the yellow house.
+		makeRule("Smoke", "Kools", "Color", "Yellow", "same"),
+		// 9. Milk is drunk in the middle house.
+		makeRule("Position", "middle", "Drink", "Milk", "same"),
+		// 10. The Norwegian lives in the first house.
+		makeRule("Position", "first", "Owner", "Norwegian", "same"),
+        // 11. The man who smokes Chesterfields lives in the house next to the man with the fox.
+		makeRule("Smoke", "Chesterfields", "Pet", "Fox", "next to"),
+        // 12. Kools are smoked in the house next to the house where the horse is kept.
+		makeRule("Smoke", "Kools", "Pet", "Horse", "next to"),
+		// 13. The Lucky Strike smoker drinks orange juice.
+		makeRule("Smoke", "Luck Strike", "Drink", "Orange Juice", "same"),
+		// 14. The Japanese smokes Parliaments.
+		makeRule("Smoke", "Parliaments", "Owner", "Japanese", "same"),
+	}
+    
+    for _, rule := range rules {
+        
+    }
 
 	return Solution{DrinksWater: "Norwegian", OwnsZebra: "Japanese"}
 }
