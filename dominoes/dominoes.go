306edec39be809d389660825103b7ad281bcd413
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-10-18 23:34:36 +0000

    First iteration dominoes

--- dominoes/dominoes.go
+++ dominoes/dominoes.go
@@ -1,7 +1,44 @@
 package dominoes
 
+type queueItem struct {
+	pool  []Domino
+	seen  []Domino
+	chain []Domino
+}
+
+func (i *queueItem) Tail() int {
+	return i.chain[len(i.chain)-1][1]
+}
+
 type Domino = [2]int
 
 func MakeChain(dominoes []Domino) ([]Domino, bool) {
-    return []Domino{}, true
+	if len(dominoes) == 0 {
+		return []Domino{}, true
+	}
+
+	queue := []queueItem{queueItem{dominoes[1:], []Domino{}, []Domino{dominoes[0]}}}
+	var item queueItem
+	var d Domino
+	for len(queue) > 0 {
+		item, queue = queue[0], queue[1:]
+		if len(item.pool) == 0 {
+			if len(item.seen) == 0 && item.chain[0][0] == item.Tail() {
+				return item.chain, true
+			}
+			continue
+		}
+		d, item.pool = item.pool[0], item.pool[1:]
+		if d[0] != item.Tail() {
+			d = Domino{d[1], d[0]}
+		}
+		if d[0] == item.Tail() {
+			queue = append(queue, queueItem{
+				append(item.pool, item.seen...),
+				[]Domino{},
+				append(item.chain, d)})
+		}
+		queue = append(queue, queueItem{item.pool, append(item.seen, d), item.chain})
+	}
+	return nil, false
 }
